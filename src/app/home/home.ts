685d42010ce2b4ac0473b15cb13eaee7356d159b
Geraldine <geraldinesaul7@gmail.com>
2026-02-22 11:11:28 +0100

    Location

---
+++ src/app/home/home.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { HousingLocation } from '../housing-location/housing-location';
+import { HousingLocationInfo } from '../housinglocation';
+
+@Component({
+  selector: 'app-home',
+  imports: [HousingLocation],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
+  housingLocation: HousingLocationInfo = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  }
+
+}
